653c0e8358f2078b74cd8aea85f426fde4fa0f8f
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2026-01-06 16:50:24 +0600

    db url moved to the env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { config } from './config/config';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://iqbalhossen60483:xfiyLUZlzj3zNV9z@cluster0.auteeh6.mongodb.net/nestjs',
-    ),
-    UserModule,
-  ],
+  imports: [MongooseModule.forRoot(config.dbUrl), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,5 @@
 export const config = {
   port: process.env.PORT ?? 8080,
   apiPrefix: process.env.API_PREFIX ?? '/api',
+  dbUrl: process.env.BD_URL ?? 'mongodb://localhost:27017/nestjs',
 };
