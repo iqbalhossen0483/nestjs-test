e0549f37bc81f3d59f79c20228d7d7c7d884d2f7
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2026-01-06 16:58:51 +0600

    dot env package added

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,8 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export const config = {
   port: process.env.PORT ?? 8080,
   apiPrefix: process.env.API_PREFIX ?? '/api',
-  dbUrl: process.env.BD_URL ?? 'mongodb://localhost:27017/nestjs',
+  dbUrl: process.env.MONGODB_URL ?? 'mongodb://localhost:27017/nestjs',
 };
