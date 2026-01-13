d620d8f4571f68c4dca194e48a89251d7fec3e84
Iqbal Hossen <iqbalhossen60483@gmail.com>
2026-01-13 16:06:46 +0600

    user service renamed

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,31 +12,31 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
-export class AuthController {
-  constructor(private readonly authService: UserService) {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
 
   @Post()
   create(@Body() createAuthDto: CreateUserDto) {
-    return this.authService.create(createAuthDto);
+    return this.userService.create(createAuthDto);
   }
 
   @Get()
   findAll() {
-    return this.authService.findAll();
+    return this.userService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.authService.findOne(id);
+    return this.userService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAuthDto: UpdateUserDto) {
-    return this.authService.update(id, updateAuthDto);
+    return this.userService.update(id, updateAuthDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.authService.remove(id);
+    return this.userService.remove(id);
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 import { User, UserSchema } from './entities/user.entity';
-import { AuthController } from './user.controller';
+import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  controllers: [AuthController],
+  controllers: [UserController],
   providers: [UserService],
 })
 export class UserModule {}
